0c5c4eaa7f3251af075362c1ce0b1dda9786e615
Michael Bischoff <Michael.Bischoff@Bertelsmann.de>
2022-07-29 12:55:57 +0200

    remove unused folders, part 2

--- converters/get2ret.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# gres - grep and substitute get$ to set$
-for file in `find $1 -type f`
-do
-    sed -e '1,$s/get\$/ret\$/g' < $file > s && mv s $file
-done
--- de.jpaw.allplugins/set-others.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Usage: set-others (oldversion) (newversion)"
-if [ x$2 = x ]; then
-    exit
-fi
-./gres $1 $2 ../*/META-INF/MANIFEST.MF
-./gres $1 $2 ../*/feature.xml ../de.jpaw.updatesite/*.xml
